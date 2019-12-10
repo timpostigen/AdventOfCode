dd305e753539f97d39f1ec7de11d99642cbb74df
Tim Postlewaite <timjpost@gmail.com>
2019-12-09 18:08:23 -0800

    verified testing

--- test_day_1_solution.py
+++ test_day_1_solution.py
@@ -2,7 +2,7 @@ import unittest   # The test framework
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_get_fuel_amount(self):
-        self.assertEqual(1,2)
+        self.assertEqual(1,1)
 
 
         
