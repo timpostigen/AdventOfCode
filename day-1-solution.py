01d72cb25f17596209f0bc1f07ef0f8c0cb7dcca
Tim Postlewaite <timjpost@gmail.com>
2019-12-09 18:07:26 -0800

    repo initialization and first problem

---
+++ day-1-solution.py
@@ -0,0 +1,4 @@
+from math import floor
+
+def get_fuel_amount(moduleMass):
+    return floor(moduleMass/2) - 2
---
+++ test_day_1_solution.py
@@ -0,0 +1,10 @@
+import unittest   # The test framework
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_get_fuel_amount(self):
+        self.assertEqual(1,2)
+
+
+        
+if __name__ == '__main__':
+    unittest.main()
