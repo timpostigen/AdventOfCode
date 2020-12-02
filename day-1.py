89034824d877c41c9c219860680338dbcabf0594
Tim Postlewaite <timjpost@gmail.com>
2020-12-01 18:12:25 -0800

    removed pasted code

--- day-1.py
+++ day-1.py
@@ -9,7 +9,7 @@ def productOfSumMatchingEntries(targetSum, expenses):
 
         for expense in expenses[i+1:]:
             expenseSum = base_expense + expense
-            
+
             if expenseSum == targetSum:
                 return base_expense * expense
 
@@ -22,24 +22,6 @@ with open('day-1-input.txt') as csvfile:
     reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
     for row in reader:
             expenses.append(row)
+
 print(len(expenses))
 print(productOfSumMatchingEntries(targetSum, expenses))
-
-
-def productOfSumMatchingEntries(targetSum, expenses):
-    # Optimization: sort then cutoff
-
-    for i in range(expenses.__len__):
-        base_expense = expenses[i]
-
-        for expense in expenses[i:]:
-            
-            # if base_expense + expense == targetSum:
-            #     return base_expense * expense
-
-
-targetSum = 2020
-expenses = [1721, 979, 366, 299, 675, 1456]
-print(expenses)
-
-# productOfSumMatchingEntries(targetSum, expenses)
