1b86708db828b05a650b1f6a0eef9db6e0bb6da5
Tim Postlewaite <timjpost@gmail.com>
2020-12-05 14:31:33 -0800

    add test method named based testing

--- 2020/day_3_test.py
+++ 2020/day_3_test.py
@@ -1,20 +1,57 @@
+import re
+from ast import literal_eval
 from unittest import TestCase
 from utility import read_lines
-from day_3 import TobboganTrajectory
+import inspect
+from day_3 import ToboganTrajectory
 
-class TestTobboganTrajectory(TestCase):
+class TestToboganTrajectory(TestCase):
+    def setUp(self):        
+        self.test_name_labels = {
+            'right': 'right_distance',
+            'down': 'down_distance',
+            'limit': None,
+            'expected': None,
+            'input': None
+        }
 
-    def test_count_trees_3(self):
+    def gen_test_config(self, test_name):
+        """
+        docstring
+        """
+        
+        test_config = {'input_file': 'day_3_input.txt'}
+
+        for label, param_name in self.test_name_labels.items():
+            match = re.search(rf'_{label}(\w+?)_', test_name)
+
+            if param_name:
+                label = param_name
+
+            if(match):
+                test_config[label] = literal_eval(match.group(1))
+
+        return test_config
+
+    def test_count_trees_1(self):
+        """
+        docstring
+        """
+        pass
+
+    def test_count_trees_right3_limit40_expected259_(self):
         # Arrange
-        input_file = 'day_3_input.txt'
+        test_name = inspect.currentframe().f_code.co_name
 
-        tt = TobboganTrajectory(
-            base_map = read_lines(input_file)
+        test_config = self.gen_test_config(test_name)
+
+        tt = ToboganTrajectory(
+            base_map = read_lines(test_config['input_file'])
         )
         
         # Act
-        actual = tt.countTrees(3)
+        actual = tt.count_trees(**test_config)
 
         # Assert
-        expected = 259
+        expected = test_config['expected']
         self.assertEqual(expected, actual)
