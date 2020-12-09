53533515ae167c4cafca56531b9ddc4be0077e42
Tim Postlewaite <timjpost@gmail.com>
2020-12-09 13:26:43 -0800

    better specify name of validation window

--- 2020/day_9.py
+++ 2020/day_9.py
@@ -52,7 +52,13 @@ class ToboganTrajectory:
     The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
     '''
     #endregion
-    
+    def __init__(self, previous_window=25):
+        """
+        docstring
+        """
+        self.previous_window = previous_window
+
+
     def find_outlier(self):
         """
         docstring
