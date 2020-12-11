824e3cb04becea14a0f060062279f2930e4abeb0
Tim Postlewaite <timjpost@gmail.com>
2020-12-10 21:38:10 -0800

    d4p2 solved

--- 2020/day_4.py
+++ 2020/day_4.py
@@ -1,5 +1,7 @@
+from collections import OrderedDict
 from pathlib import Path
 from utility import read_lines
+import re
 
 class PassportProcessing:
     # region --- Day 4: Passport Processing ---
@@ -51,23 +53,160 @@ class PassportProcessing:
 
     Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
 
+    Notes:
+    249 < answer < 282 (max)
+
+    --- Part Two ---
+
+    The line is moving more quickly now, but you overhear airport security talking about how passports with invalid data are getting through. Better add some data validation, quick!
+
+    You can continue to ignore the cid field, but each other field has strict rules about what values are valid for automatic validation:
+
+        byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        hgt (Height) - a number followed by either cm or in:
+            If cm, the number must be at least 150 and at most 193.
+            If in, the number must be at least 59 and at most 76.
+        hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        pid (Passport ID) - a nine-digit number, including leading zeroes.
+        cid (Country ID) - ignored, missing or not.
+
+    Your job is to count the passports where all required fields are both present and valid according to the above rules. Here are some example values:
+
+    byr valid:   2002
+    byr invalid: 2003
+
+    hgt valid:   60in
+    hgt valid:   190cm
+    hgt invalid: 190in
+    hgt invalid: 190
+
+    hcl valid:   #123abc
+    hcl invalid: #123abz
+    hcl invalid: 123abc
+
+    ecl valid:   brn
+    ecl invalid: wat
+
+    pid valid:   000000001
+    pid invalid: 0123456789
+
+    Here are some invalid passports:
+
+    eyr:1972 cid:100
+    hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+    iyr:2019
+    hcl:#602927 eyr:1967 hgt:170cm
+    ecl:grn pid:012533040 byr:1946
+
+    hcl:dab227 iyr:2012
+    ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+    hgt:59cm ecl:zzz
+    eyr:2038 hcl:74454a iyr:2023
+    pid:3556412378 byr:2007
+
+    Here are some valid passports:
+
+    pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+    hcl:#623a2f
+
+    eyr:2029 ecl:blu cid:129 byr:1989
+    iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+    hcl:#888785
+    hgt:164cm byr:2001 iyr:2015 cid:88
+    pid:545766238 ecl:hzl
+    eyr:2022
+
+    iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+
+    Count the number of valid passports - those that have all required fields and valid values. Continue to treat cid as optional. In your batch file, how many passports are valid?
+
+    Notes:
+
+    answer < 162
     """
     # endregion
-
-    def count_valid_passports(self):
+    def __init__(self, raw_passport_feed):
         """
         docstring
         """
-        pass
+        self.raw_passport_feed = raw_passport_feed
+        self.required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+        self.optional_fields = ['cid']
+
+    def parse_passport_feed(self, feed):
+        passports = []
+        passport = {}
+
+        for line in feed:
+            if not line:
+                passports.append(OrderedDict(sorted(passport.items())))
+                passport = {}
+                continue
+            
+            # repeat field keys?
+
+            fields = re.split(r'\s+', line)
+
+            for field in fields:
+                k, v = field.split(':')
+                passport[k] = v
+
+        return passports
+    
+    def count_valid_passports(self, passports):
+        """
+        docstring
+        """
+        valid_passports = 0
+
+        for passport in passports:
+            if(self.validate_passport(passport)):
+                valid_passports += 1
+
+        return valid_passports
 
     def validate_passport(self, passport):
         """
         docstring
         """
-        pass
-    pass
+        valid = False
+
+        if(all(item in passport.keys() for item in self.required_fields)):
+            four_digits = r'\d{4}'
+            height_match = re.match(r'(\d+)(cm|in)', passport['hgt'])
+
+            if(height_match):
+                value = int(height_match.groups()[0])
+                units = height_match.groups()[1]
+
+                if(units == 'cm'):
+                    valid = 150 <= value <= 193
+                elif(units == 'in'):
+                    valid = 59 <= value <= 76
+
+            valid = valid and re.match(four_digits, passport['byr']) and 1920 <= int(passport['byr']) <= 2002
+            valid = valid and passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+            valid = valid and re.match(four_digits, passport['eyr']) and 2020 <= int(passport['eyr']) <= 2030
+            valid = valid and re.match(r'#[0-9a-f]{6}', passport['hcl'])
+            valid = valid and re.match(four_digits, passport['iyr']) and 2010 <= int(passport['iyr']) <= 2020
+            valid = valid and re.match(r'\d{9}', passport['pid'])
+
+        return valid
 
 if __name__ == "__main__":
-    passports = read_lines(f'{Path(__file__).stem}_input.txt')
+    raw_passport_feed = read_lines(f'{Path(__file__).stem}_input.txt')
 
-    
+    pp = PassportProcessing(raw_passport_feed)
+
+    passports = pp.parse_passport_feed(pp.raw_passport_feed)
+    print(len(passports))
+
+    valid_passports = pp.count_valid_passports(passports)
+
+    print(valid_passports)
