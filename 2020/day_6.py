75cc6aab5d5e75b23643dd30fd5b6ee404946aa8
Tim Postlewaite <timjpost@gmail.com>
2020-12-13 13:28:56 -0800

    used sets/intersections for complete solve

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -1,5 +1,6 @@
 from utility import get_input_file
 from pathlib import Path
+from string import ascii_lowercase
 
 class CustomCustoms():
     #region --- Day 6: Custom Customs ---
@@ -47,12 +48,107 @@ class CustomCustoms():
     In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
 
     For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+    6683
+
+    --- Part Two ---
+
+    As you finish the last group's customs declaration, you notice that you misread one word in the instructions:
+
+    You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+
+    Using the same example as above:
+
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+
+    This list represents answers from five groups:
+
+        In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+        In the second group, there is no question to which everyone answered "yes".
+        In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+        In the fourth group, everyone answered yes to only 1 question, a.
+        In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+
+    In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+    For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
+
+
+    Notes:
+    6668 < answer < 7151
 
     """
     #endregion
     
-    pass
+    def tally_questions(self):
+        """
+        docstring
+        """
 
+        input_file_name =  Path(__file__).parent / f'{Path(__file__).stem}_input.txt'
+
+        questions = set()
+
+        total_questions = 0
+
+        with open(input_file_name) as file:
+            for line in file:
+                line = line.strip()
+                
+                if not line:
+                    total_questions += len(questions)
+                    questions = set()
+
+                for question in line:
+                    questions.add(question)
+
+        total_questions += len(questions)
+
+        return total_questions
+
+    def tally_all_answered(self):
+        """
+        docstring
+        """
+
+        input_file_name =  Path(__file__).parent / f'{Path(__file__).stem}_input.txt'
+
+        # since we're using intersection, let's start with all questions instead of no questions
+        group_questions = all_questions = set(list(ascii_lowercase))
+        total_questions = 0
+
+        with open(input_file_name) as file:
+            for line in file:
+                line = line.strip()
+                
+                # new group
+                if not line:
+                    total_questions += len(group_questions)
+                    group_questions = all_questions
+                    continue
+
+                group_questions = set(list(line)).intersection(group_questions)
+
+        total_questions += len(group_questions)
+
+        return total_questions
 
 if __name__ == "__main__":
-    print(get_input_file(f'{Path(__file__).stem}_input.txt'))
+    cc = CustomCustoms()
+    
+    print("Tally questions: ", cc.tally_questions())
+
+    print("Tally all answered: ", cc.tally_all_answered())
